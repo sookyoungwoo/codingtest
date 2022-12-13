f7a0abafc632767a38bcd225ae46782b93f4f853
sookyoungwoo <sookyoung2005@gmail.com>
2022-12-13 17:28:20 +0900

    SUCCESS :: 맞은 줄 알았지만 마이너스 잘못 붙였던

--- backjoon/CCW/index.py
+++ backjoon/CCW/index.py
@@ -6,5 +6,5 @@ x3,y3 = map(int, input().split())
 result = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
 
 if result == 0 : print("0")
-elif result > 0 : print("-1")
-else : print("1")
+elif result > 0 : print("1")
+else : print("-1")
